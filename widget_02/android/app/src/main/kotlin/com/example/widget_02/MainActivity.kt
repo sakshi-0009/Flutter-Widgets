0ebd709bf65ee46acef0f65d084b9e97cf4198d3
Sakshi <sakshigome1809@gmail.com>
2025-03-15 11:03:44 +0530

    Drawer Widget

---
+++ widget_02/android/app/src/main/kotlin/com/example/widget_02/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.widget_02
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ widget_02/lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
+
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey, // Assign key to Scaffold
+      appBar: AppBar(
+        title: const Text("Student Data Management"),
+        backgroundColor: Colors.orange,
+        centerTitle: true,
+        leading: IconButton(
+          icon: const Icon(Icons.menu), // Hamburger icon
+          onPressed: () {
+            _scaffoldKey.currentState?.openDrawer();
+          },
+        ),
+      ),
+      drawer: Drawer(
+        width: 200,
+        backgroundColor: Colors.orangeAccent,
+        child: Column(
+          children: [
+            const DrawerHeader(
+              child: Text(
+                "DashBoard",
+                style: TextStyle(fontSize: 25, color: Colors.white),
+              ),
+            ),
+            drawerItem(Icons.person, "Profile"),
+            drawerItem(Icons.fastfood, "Food"),
+            drawerItem(Icons.note_add, "Notes"),
+            drawerItem(Icons.settings, "Settings"),
+            drawerItem(Icons.logout, "Logout"),
+          ],
+        ),
+      ),
+      body: const Center(
+        child: Text("Home Page", style: TextStyle(fontSize: 20)),
+      ),
+    );
+  }
+
+  // Drawer Item Widget for reusability
+  Widget drawerItem(IconData icon, String title) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      child: Row(
+        children: [
+          Icon(icon, color: Colors.white),
+          const SizedBox(width: 15),
+          Text(title, style: const TextStyle(color: Colors.white)),
+        ],
+      ),
+    );
+  }
+}
