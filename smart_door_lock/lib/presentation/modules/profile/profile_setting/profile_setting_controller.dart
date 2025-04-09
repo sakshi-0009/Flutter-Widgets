db328bd63a1a2ed2fcf7d26bf28b3bd0ae0a5e93
Sakshi <sakshigome1809@gmail.com>
2025-04-09 11:02:57 +0530

    Updated Profile Setting Page

--- smart_door_lock/lib/presentation/modules/profile/profile_setting/profile_setting_controller.dart
+++ smart_door_lock/lib/presentation/modules/profile/profile_setting/profile_setting_controller.dart
@@ -1,5 +1,110 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class ProfileSettingController extends GetxController {
+
+  RxString addPhoto = 'Gallery'.obs;
+
+  void showAddPhotoSheet() {
+    Get.snackbar(
+      '',
+      '',
+      backgroundColor: Colors.white,
+      snackPosition: SnackPosition.BOTTOM,
+      duration: const Duration(seconds: 2),
+      margin: const EdgeInsets.all(10),
+      messageText: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: const Color(0xFFFFFFFF),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          children: [
+            // Header Row
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  "Add photo",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
+                GestureDetector(
+                  onTap: () => Get.back(), // closes snackbar
+                  child: const Icon(Icons.close),
+                ),
+              ],
+            ),
+            const SizedBox(height: 16),
+
+            // Gallery / Camera options
+            Obx(() => Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    Get.find<ProfileSettingController>().addPhoto.value = 'Gallery';
+                  },
+                  child: Container(
+                    width: 140,
+                    padding: const EdgeInsets.symmetric(vertical: 12),
+                    decoration: BoxDecoration(
+                      color: Get.find<ProfileSettingController>().addPhoto.value == 'Gallery'
+                          ? Colors.red.shade700
+                          : Colors.white,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Column(
+                      children: [
+                        Icon(Icons.photo),
+                        const SizedBox(height: 4),
+                        Text(
+                          "Gallery",
+                          style: TextStyle(
+                            color: Get.find<ProfileSettingController>().addPhoto.value == 'Gallery'
+                                ? Colors.white
+                                : Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    Get.find<ProfileSettingController>().addPhoto.value = 'Camera';
+                  },
+                  child: Container(
+                    width: 140,
+                    padding: const EdgeInsets.symmetric(vertical: 12),
+                    decoration: BoxDecoration(
+                      color: Get.find<ProfileSettingController>().addPhoto.value == 'Camera'
+                          ? Colors.red.shade700
+                          : Colors.white,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Column(
+                      children: [
+                        Icon(Icons.camera_alt_sharp),
+                        const SizedBox(height: 4),
+                        Text(
+                          "Camera",
+                          style: TextStyle(
+                            color: Get.find<ProfileSettingController>().addPhoto.value == 'Camera'
+                                ? Colors.white
+                                : Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            )),
+          ],
+        ),
+      ),
+    );
+  }
 
 }
--- smart_door_lock/lib/presentation/modules/profile/profile_setting/profile_setting_view.dart
+++ smart_door_lock/lib/presentation/modules/profile/profile_setting/profile_setting_view.dart
@@ -1,18 +1,48 @@
-import 'package:europa_lock/presentation/modules/profile/subscription/subscription_controller.dart';
+import 'package:europa_lock/presentation/modules/profile/profile_setting/profile_setting_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class ProfileSettingView extends GetView<SubscriptionController> {
+class ProfileSettingView extends GetView<ProfileSettingController> {
   const ProfileSettingView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Profile Settings"),
+        title: const Text("Edit Profile"),
       ),
-      body: const Center(
-        child: Text("Profile Settings"),
+      body: Center(
+        child: Column(
+          children: [
+            // Circular Profile Container
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                Container(
+                  width: 130,
+                  height: 130,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.black12, // You can show profile image here
+                  ),
+                ),
+                Positioned(
+                  child: GestureDetector(
+                    onTap: controller.showAddPhotoSheet,
+                    child: const Text(
+                      "Add photo",
+                      style: TextStyle(
+                        color: Colors.red,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
